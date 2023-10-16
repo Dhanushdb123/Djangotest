1c36e2ca6f11bfc4f8aefa26dfa198d54906a2d5
unknown <dhanush@caritorsolutions.in>
2023-10-16 20:45:32 +0530

    First commit

---
+++ plaindjango/views.py
@@ -0,0 +1,9 @@
+from django.http.response import HttpResponse
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def pallabdef(*args, **kwargs):
+    #print("akai")
+    #return HttpResponse("<h1>Akai is a bad boy<h1>")
+    return HttpResponse("<h1>Ishaan is a naughty boy<h1>")
